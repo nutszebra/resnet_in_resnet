fa758364952f1e45cb14086dd11db85a7664ae96
nutszebra <aiskoaskosd@gmail.com>
2017-01-19 11:16:04 +0900

    changed total epoch

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
                         default='./',
                         help='model and optimizer will be saved every epoch')
     parser.add_argument('--epoch', '-e', type=int,
-                        default=82,
+                        default=300,
                         help='maximum epoch')
     parser.add_argument('--batch', '-b', type=int,
                         default=500,
